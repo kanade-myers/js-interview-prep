9a078ec079ad3225269d8d5aa28e3ea2e194c840
Кирилл <146585768+kanade-myers@users.noreply.github.com>
2026-02-06 15:32:46 +0700

    Add files via upload
    Решены следующие задачи:
    1. создание функции конструктора для калькулятора
    2. реализация метода добавления новых операций в калькулятор

---
+++ calculator.js
@@ -0,0 +1,32 @@
+// Калькулятор с добавляемыми методами
+
+function Calculator() {
+    this.methods = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b,
+    };
+
+    this.calculate = function (str) {
+        let split = str.split(" ");
+
+        a = +split[0];
+        operator = split[1];
+        b = +split[2];
+
+        if (isNaN(a) || isNaN(b) || !this.methods[operator]) {
+            return NaN;
+        }
+        return this.methods[operator](a, b);
+    };
+    this.addMethod = function (name, func) {
+        this.methods[name] = func;
+    };
+}
+
+let powerCalc = new Calculator();
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+console.log(result); // 8
